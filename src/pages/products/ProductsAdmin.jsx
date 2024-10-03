e3ac580008b04d19ce1ae5a213ae520d56f74734
Rhazes Devino <rhazesd@gmail.com>
2024-10-03 21:38:47 +0700

    create product crud feature

---
+++ src/pages/products/ProductsAdmin.jsx
@@ -0,0 +1,23 @@
+import DashboardBanner from "../../components/common/cards/DashboardBanner";
+import Layout from "../../components/common/layouts/LayoutAdmin";
+import ProductList from "./sections/ProductList";
+
+function ProductsAdmin() {
+  return (
+    <Layout>
+      <main className="flex flex-col gap-4 ">
+        <div className="p-4">
+          <DashboardBanner
+            title="Products Page"
+            message="Welcome to the Products Page. Browse through our extensive collection of products, view detailed information, and manage your inventory seamlessly. For any assistance, our support team is here to help."
+          />
+        </div>
+        <div className="px-4">
+          <ProductList />
+        </div>
+      </main>
+    </Layout>
+  );
+}
+
+export default ProductsAdmin;
